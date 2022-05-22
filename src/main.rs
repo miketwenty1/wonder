7d1d902b8714bbef16e84c645bc869b60decf73f
Michael Tidwell <tidwell@zebedee.io>
2022-05-22 18:46:09 -0400

    bevy wonder

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,57 @@
-use bevy::{prelude::*, text::Text2dBounds};
+#![allow(clippy::redundant_field_names)]
+use bevy::{prelude::*, render::camera::ScalingMode, window::PresentMode};
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+mod player;
+use player::PlayerPlugin;
 
 fn main() {
+    let height = 900.0;
     App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: height * RESOLUTION,
+            height: height,
+            title: "Bevy Tutorial".to_string(),
+            present_mode: PresentMode::Fifo,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_startup_system(spawn_camera)
+        // .add_startup_system(spawn_player)
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(change_text_of_block)
+        .add_plugins(PlayerPlugin)
         .run();
 }
 
-#[derive(Component)]
-struct TextChanges;
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+    camera.orthographic_projection.left = -1.0 * RESOLUTION;
+    camera.orthographic_projection.right = 1.0 * RESOLUTION;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-    let text_style = TextStyle {
-        font,
-        font_size: 60.0,
-        color: Color::WHITE,
-    };
-    // 2d camera
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    let box_size = Size::new(300.0, 300.0);
-    let box_position = Vec2::new(0.0, 0.0);
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(box_size.width, box_size.height)),
-            ..default()
-        },
-        transform: Transform::from_translation(box_position.extend(0.0)),
-        ..default()
-    });
-    let text_alignment_center = TextAlignment {
-        vertical: VerticalAlign::Center,
-        horizontal: HorizontalAlign::Center,
-    };
-    commands
-        .spawn_bundle(Text2dBundle {
-            text: Text::with_section("0", text_style, text_alignment_center),
-            text_2d_bounds: Text2dBounds {
-                // Wrap text in the rectangle
-                size: box_size,
-            },
-            // We align text to the top-left, so this transform is the top-left corner of our text. The
-            // box is centered at box_position, so it is necessary to move by half of the box size to
-            // keep the text in the box.
-            // transform: Transform::from_xyz(
-            //     box_position.x - box_size.width / 2.0,
-            //     box_position.y + box_size.height / 2.0,
-            //     1.0,
-            // ),
-            ..default()
-        })
-        .insert(TextChanges);
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
 }
 
-fn change_text_of_block(time: Res<Time>, mut query: Query<&mut Text, With<TextChanges>>) {
-    for mut text in query.iter_mut() {
-        let block_str = (text.sections[0].value).to_string();
-        let block_int: i32 = block_str.parse().unwrap();
-        //let block_fin = block_int
+struct AsciiSheet(Handle<TextureAtlas>);
 
-        //println!("{}", block_int);
-        text.sections[0].value = format!("{}", block_int + 1);
-    }
+fn load_ascii(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let image = assets.load("Ascii.png");
+
+    let atlas =
+        TextureAtlas::from_grid_with_padding(image, Vec2::splat(9.0), 16, 16, Vec2::splat(2.0));
+
+    let atlas_handle = texture_atlases.add(atlas);
+
+    commands.insert_resource(AsciiSheet(atlas_handle));
 }
